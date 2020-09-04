4ba54e9b3f22fe1c6739698ab4e64a05d83b1cef
Leticia Massenan <massenandev@gmail.com>
2020-09-04 17:59:34 -0300

    layout settings

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="description" content="@yield('page_description', 'A cool social network to share your hobbies')">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('page_title', 'My Hobbies')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -36,7 +38,8 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                        <li><a class="nav-link{{ Request::is('/') ? ' active' : '' }}" href="/">Start</a></li>
+                        <li><a class="nav-link{{ Request::is('info') ? ' active' : '' }}" href="/info">Info</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
---
+++ resources/views/starting_page.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('page_description')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Starting Page</div>
+
+                <div class="card-body">
+                    Qu'est-ce que le Lorem Ipsum?
+                    Le Lorem Ipsum est simplement du faux texte
+                    employé dans la composition et la mise en page a
+                    vant impression. Le Lorem Ipsum est le faux texte s
+                    tandard de l'imprimerie depuis les années 1500, quand
+                    un imprimeur anonyme assembla ensemble des morceaux
+                    de texte pour réaliser un livre spécimen de polices
+                    de texte. Il n'a pas fait que survivre cinq siècles,
+                    mais s'est aussi adapté à la bureautique
+                    informatique, sans que son contenu n'en soit modifié.
+                    Il a été popularisé dans les années 1960 grâce à la
+                    vente de feuilles Letraset contenant des passages du
+                    Lorem Ipsum, et, plus récemment, par son inclusion
+                    dans des applications de mise en page de texte, comme Aldus PageMaker.
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
